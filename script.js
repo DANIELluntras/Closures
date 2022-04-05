099cc706acf29835757ce662d0fd6991acd7de8d
luntras daniel <99122608+DANIELluntras@users.noreply.github.com>
2022-04-05 14:59:20 +0300

    am adugat un fiser

---
+++ script.js
@@ -0,0 +1,17 @@
+//////////////////////////////=137
+//137. Closures
+const dani = function () {
+    let pasager = 0;
+    return function () {
+        pasager++;
+        console.log(`${pasager} acesta este passaportul `);
+    }
+}
+const booker = dani();
+booker();
+booker();
+booker();
+booker();
+booker();
+booker();
+booker();  
