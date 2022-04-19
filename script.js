545d24cb5e77e2a0497f959143ec91e2c43a50a9
luntras daniel <99122608+DANIELluntras@users.noreply.github.com>
2022-04-19 23:58:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,14 @@ booker();
 booker();
 booker();
 booker();  
+// style 
+// this is style begginer  , I like 
+
+(function() {
+    const header = document.querySelector('h1');
+    header.style.color = 'blue'
+
+    document.querySelector('body').addEventListener('click', function() {
+        header.style.color = 'black'
+    })
+})();
